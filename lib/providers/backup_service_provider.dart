a085d740619f40d6d1c9ea4bc4043754541fdd20
Pascal <pascal@example.com>
2026-04-22 23:46:14 +0200

    feat: add BackupService riverpod provider

---
+++ lib/providers/backup_service_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summsumm/services/backup_service.dart';
+import 'package:summsumm/services/meeting_repository.dart';
+import 'package:summsumm/services/secure_storage_service.dart';
+
+import 'meeting_repository_provider.dart';
+import 'settings_provider.dart';
+
+final backupServiceProvider = Provider<BackupService>((ref) {
+  final meetingRepository = ref.watch(meetingRepositoryProvider);
+  final secureStorage = ref.watch(secureStorageProvider);
+  
+  return BackupService(
+    meetingRepository: meetingRepository,
+    secureStorage: secureStorage,
+    getSettings: () => ref.read(settingsProvider),
+    onSettingsImported: (settings) {
+      ref.read(settingsProvider.notifier).persistSettingsDirect(settings);
+    },
+  );
+});
